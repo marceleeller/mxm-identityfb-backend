a79a4ff3b81b25f4787fb464171202bd5e8be43c
Marcele Eller <dev.marcele@gmail.com>
2024-04-19 16:50:27 -0300

    feat(apidocumentation): documentation

---
+++ MxmIdentityfbBackend/Domain/Dtos/UserResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MxmIdentityfbBackend.Domain.Dtos;
+
+public class UserResponseDto
+{
+    public string Email { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
